4712c07dc509c17ba0c2966ee33bef9bcd0fc93c
Zengxianglin <1075433609@qq.com>
2016-10-03 19:28:46 +0800

    2016/10/3

--- src/js/components/chatList.js
+++ src/js/components/chatList.js
@@ -6,18 +6,34 @@ export class ChatList extends Component {
 	    super(props);
 	}
 	componentDidMount() {
+		const userList =  this.refs.userList;
+		userList.addEventListener('click',this.handleClick,false);
+		//console.log(userList.childNodes);
+	}
+	handleClick(e) {
+		e = e || window.event;//这一行及下一行是为兼容IE8及以下版本
+		var target = e.target || e.srcElement;
+		console.log(e.target.nodeName)
+		if(e.target && e.target.nodeName == 'LI') {
+	    	
+	    }
 	}
 	render() {
 		return (<div className='react-wrap'>
 					<div className='user-search'>
 					</div>
-					<ul className='user-list'>
-						<li className='public-room'>
+					<ul className='user-list' ref='userList'>
+						<li className='public-room' id='li1'>
+							<img src='' />
+							<span>username</span>
+							<p>msgmsgmsgmsgmsgmsgmsgmsgmsg</p>
+						</li>
+						<li className='private-room' id='li2'>
 							<img src='' />
 							<span>username</span>
 							<p>msg</p>
 						</li>
-						<li className='private-room'>
+						<li className='private-room' id='li3'>
 							<img src='' />
 							<span>username</span>
 							<p>msg</p>
