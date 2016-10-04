d51b9b21488e36d94645274300ecbe88667e2c1e
Zengxianglin <1075433609@qq.com>
2016-10-04 20:58:18 +0800

    2016/10/4

--- socket_server.js
+++ socket_server.js
@@ -1,10 +1,46 @@
 module.exports = function(server) {
+
+	var users = {};
+	var usersockets = {};
+
 	var io = require('socket.io').listen(server);
 	io.sockets.on('connection', function(socket){
-	  console.log('connect success.')
-	  socket.on('postMsg', function(name, msg) {
-	      console.log('postMsg');
-	      io.sockets.emit('newMsg', name, msg);
-	  });
+	    console.log('connect success.');
+	    socket.on('user join', function(data) {
+		    users[username] = username;
+		    usocket[username] = socket;
+	    })
+	    socket.on('postMsg', function(name, msg) {
+	        console.log('postMsg');
+	        io.sockets.emit('newMsg', name, msg);
+	    });
+	    
 	})
+	io.sockets.on('disconnect', function() {
+		console.log('disconnect')
+	    // if (username) {
+	    //     counter--;
+	    //     delete users[username];
+	    //     delete usocket[username];
+	    //     if (home.name == username) {
+	    //         homeLeave(username);
+	    //     }
+	    //     sendmsg({
+	    //         type: 0,
+	    //         msg: "用户<b>" + username + "</b>离开聊天室",
+	    //         counter: counter,
+	    //         users: users
+	    //     })
+	    // }
+	});
+
+	function sendUserMsg(data) {
+	    if (data.to in usersocket) {
+	        console.log('================')
+	        console.log('to' + data.to, data);
+	        usocket[data.to].emit('to' + data.to, data);
+	        usocket[data.user].emit('to' + data.user, data);
+	        console.log('================')
+	    }
+	}
 }
