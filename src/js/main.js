78622b9c3a5148ab062ac799420ef7e2b14704a0
Zengxianglin <1075433609@qq.com>
2016-10-27 17:49:29 +0800

    Object.assign方法兼容

--- src/js/main.js
+++ src/js/main.js
@@ -29,6 +29,32 @@ export const PHOTO = photo;
 // }
 
 //将state相关属性绑定到props的中
+
+
+//兼容手机UC，QQ和IE
+if (typeof Object.assign != 'function') {
+  (function () {
+	Object.assign = function (target) {
+	 'use strict';
+	 if (target === undefined || target === null) {
+	   throw new TypeError('Cannot convert undefined or null to object');
+	 }
+	 var output = Object(target);
+	 for (var index = 1; index < arguments.length; index++) {
+	   var source = arguments[index];
+	   if (source !== undefined && source !== null) {
+	     for (var nextKey in source) {
+	       if (source.hasOwnProperty(nextKey)) {
+	         output[nextKey] = source[nextKey];
+	       }
+	     }
+	   }
+	 }
+	 return output;
+	};
+})();
+}
+
 function mapStateToprops(state){
 	return {
 		users: state.users,
